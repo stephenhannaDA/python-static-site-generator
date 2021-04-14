514a82585e43392d478413f70eba20bd73678f5d
stephenhannaDA <73045342+stephenhannaDA@users.noreply.github.com>
2021-04-14 05:38:35 -0700

    Update site.py

--- ssg/site.py
+++ ssg/site.py
@@ -16,14 +16,14 @@ class Site:
         for path in self.source.rglob("*"):
             if path.is_dir():
                 self.create_dir(path)
-                
-            elif path.isfile():
-            	self.run_parser(path)
+            elif path.is_file():
+                self.run_parser(path)
 
     def load_parser(self, extension):
     	for parser in self.parsers:
     		if parser.valid_extension(extension):
     			return parser
+    			
     def run_parser(self, path):
     	parser = self.load_parser(path.suffix)
     	if parser is not None:
