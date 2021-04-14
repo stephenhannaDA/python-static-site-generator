e632024246bff75b0cd87b80879442a7f83e9e01
stephenhannaDA <73045342+stephenhannaDA@users.noreply.github.com>
2021-04-14 05:32:08 -0700

    Update site.py

--- ssg/site.py
+++ ssg/site.py
@@ -17,16 +17,16 @@ class Site:
             if path.is_dir():
                 self.create_dir(path)
             elif path.isfile():
-            	Site.run_parser(path)
+            	self.run_parser(path)
 
     def load_parser(self, extension):
     	for parser in self.parsers:
-    		if extension.valid_extension():
+    		if parser.valid_extension(extension):
     			return parser
     def run_parser(self, path):
     	parser = self.load_parser(path.suffix)
-    	if parser:
-    		parser.parse(path, source, dest)
+    	if parser is not None:
+    		parser.parse(path, self.source, self.dest)
     	else:
     		print("Not Implemented")
 
