e2ea10713929867269418edde0902ba36436f334
stephenhannaDA <73045342+stephenhannaDA@users.noreply.github.com>
2021-04-17 02:15:41 -0700

    Update parsers.py

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -44,7 +44,7 @@ class MarkdownParser(Parser):
 		sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n").format(path.name, content)
 
 class ReStructuredTextParser(Parser):
-	extension = [".rst"]
+	extensions = [".rst"]
 
 	def parse(self, path, source, dest):
 		content = Content.load(self.read(path))
