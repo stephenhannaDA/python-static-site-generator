d07c77aa6c3df1bdbdf913b7fc2c850b945e2115
stephenhannaDA <73045342+stephenhannaDA@users.noreply.github.com>
2021-04-17 02:25:54 -0700

    Update site.py

--- ssg/site.py
+++ ssg/site.py
@@ -26,14 +26,14 @@ class Site:
     		if parser.valid_extension(extension):
     			return parser
     			
-        def run_parser(self, path):
-        parser = self.load_parser(path.suffix)
-        if parser is not None:
-            parser.parse(path, self.source, self.dest)
-        else:
-            self.error(
-                "No parser for the {} extension, file skipped!".format(path.suffix)
-            )
+    def run_parser(self, path):
+    parser = self.load_parser(path.suffix)
+    if parser is not None:
+        parser.parse(path, self.source, self.dest)
+    else:
+        self.error(
+            "No parser for the {} extension, file skipped!".format(path.suffix)
+        )
 
     @staticmethod
     def error(message):
